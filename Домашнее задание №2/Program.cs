4eb81a27258e23a0b1242ceb0e812381045496cc
User <User@WIN-GIA6K9L11G8>
2021-09-02 00:44:05 +0300

    Добавьте файлы проекта.

---
+++ Домашнее задание №2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Домашнее_задание__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //Первое задание
+            string Name1 = "Сотрудник 1";  
+            Console.WriteLine(Name1);
+            byte Age1 = 23;
+            Console.WriteLine(Age1);
+            byte Height1 = 170;
+            Console.WriteLine(Height1);
+            byte BallsRus1 = 8;
+            Console.WriteLine(BallsRus1);
+            byte BallsHistory1 = 9;
+            Console.WriteLine(BallsHistory1);
+            byte Mathematics1 = 11;
+            Console.WriteLine(Mathematics1);  //Опесание первого сотрудника
+
+            string Name2 = "Сотрудник 2";
+            Console.WriteLine(Name2);
+            byte Age2 = 22;
+            Console.WriteLine(Age2);
+            byte Height2 = 175;
+            Console.WriteLine(Height2);
+            byte BallsRus2 = 9;
+            Console.WriteLine(BallsRus2);
+            byte BallsHistory2 = 9;
+            Console.WriteLine(BallsHistory2);
+            byte Mathematics2 = 10;
+            Console.WriteLine(Mathematics2);  // Опесание второго сотрудника
+
+            string Name3 = "Сотрудник 3";
+            Console.WriteLine(Name3);
+            byte Age3 = 23;
+            Console.WriteLine(Age3);
+            byte Height3 = 170;
+            Console.WriteLine(Height3);
+            byte BallsRus3 = 3;
+            Console.WriteLine(BallsRus3);
+            byte BallsHistory3 = 9;
+            Console.WriteLine(BallsHistory3);
+            byte Mathematics3 = 8;
+            Console.WriteLine(Mathematics3);  //Опесание третьего сотрудника
+           ///Мне кажется или тут что-то не так                                        
+        }
+    }
+}
